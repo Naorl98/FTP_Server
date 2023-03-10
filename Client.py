cb7efd197d08120dbcfaf2a5aeb4c409d333aea9
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2023-03-10 06:48:35 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,7 +4,6 @@ import os
 import tkinter as tk
 from tkinter import *
 
-
 ip = "127.0.0.1"
 port = 30190
 time_out = 5
@@ -19,6 +18,8 @@ def home():
     ftpw.pack_forget()
     first.pack(fill='both', expand=1)
 
+# ftp client start
+
 
 def connect():
     con.pack(fill='both', expand=1)
@@ -30,7 +31,7 @@ def connect():
             sock.sendto("NEW".encode(), (ip, port))
             time.sleep(0.02)
         except ConnectionResetError:
-            lab.config(text ="Server not open")
+            lab.config(text="Server not open")
             lab.pack(padx=10, pady=20)
             num_try = 5
         try:
@@ -46,7 +47,7 @@ def connect():
                 lab.pack(padx=10, pady=20)
                 num_wait += 1
                 time.sleep(2)
-        except ConnectionError:
+        except Exception as e:
             num_try += 1
             lab.config(text="Try again.." + str(num_try) + " time")
             time.sleep(0.2)
@@ -78,7 +79,8 @@ def upload_finish(buff):
                 sock.sendto(buff[int(send[4:])].encode(), (ip, port))
             elif send[:4].decode() == "ERR3":
                 sock.sendto("ACK_R".encode(), (ip, port))
-        except ValueError:
+        except Exception as e:
+            print(f"ERROR: {e}")
             timescount += 1
             if timescount < tries:
                 sock.sendto("ACK_R".encode(), (ip, port))
@@ -209,7 +211,7 @@ def cfile():
                     index_packet.append(num)
                     if data[:1].decode() == "L":
                         larrived = True
-        except TimeoutError:
+        except socket.timeout:
             print("Time Out")
             sock.sendto("BYE".encode(), (ip, port))
             break
@@ -227,7 +229,8 @@ def upload():
     index_send = []
     try:
         f = open(file_name, 'r')
-    except FileNotFoundError:
+    except Exception as e:
+        print(f"ERROR: {e}")
         filename.delete("1.0", "end")
         filename.insert(INSERT, "File not found")
         print("File not found")
@@ -279,23 +282,93 @@ def upload():
             else:
                 seq = "P" + str(index)
                 sock.sendto(seq.encode(), (ip, port))
+# ftp client start
+
+
+# dns client start
+
+BUFF = 512  # Buffer size
+ip_port = ("127.0.0.1", 53)
+
+
+def check_domain(domain):
+    # Split the domain to parts
+    domain_parts = domain.split('.')
+    # if it has less than 2 parts its not good : google.com has 2
+    if len(domain_parts) < 2:
+        return False
+    return True
+
+
+def domain_to_ip():
+    try:
+        domain = (link.get('1.0', tk.END))[:-1]
+        # Convert domain name to UTF-8 bytes
+        if check_domain(domain) is False:
+            link.delete("1.0", "end")
+            link.insert(INSERT, "Error with this domain name, try again")
+            link.pack(padx=10, pady=20)
+            print("ERROR WITH THIS DOMAIN NAME, TRY AGAIN")
+            return
+        msg = domain.encode('utf-8')
+        print(f"SENDING DNS QUERY FOR {domain}")
+        # Sends the query to the server
+        sock.sendto(msg, ip_port)
+
+        # Response from server
+        data, server = sock.recvfrom(BUFF)
+        ip_add = data.decode('utf-8')
+        link.config(text="Ip address -> " + ip_add)
+        link.pack(padx=10, pady=20)
+        print(f"THE IP FOR {domain} is {ip_add}")
+    # except socket.gaierror:
+    except socket.timeout:
+        link.delete("1.0", "end")
+        link.insert(INSERT, "Error with this domain name, try again")
+        link.pack(padx=10, pady=20)
+        print("ERROR WITH THIS DOMAIN NAME, TRY AGAIN")
+        return
+    except socket.error as e:
+        link.delete("1.0", "end")
+        link.insert(INSERT, "Error with this domain name, try again")
+        link.pack(padx=10, pady=20)
+        print("ERROR WITH THIS DOMAIN NAME, TRY AGAIN")
+        return
+    except Exception as e:
+        link.delete("1.0", "end")
+        link.insert(INSERT, "Error with this domain name, try again")
+        link.pack(padx=10, pady=20)
+        print("ERROR WITH THIS DOMAIN NAME, TRY AGAIN")
+        return
+
+
+def dns_client():
+    first.pack_forget()
+    dnsw.pack(fill='both', expand=1)
 
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # root start
     root = Tk()
     root.geometry("500x500")
     root.title("Final Proj")
+    # root finish
+
+    # first start
     first = Frame(root, bg='Black')
     first.pack(fill='both', expand=1)
     label = tk.Label(first, text="Choose option", fg='Green', bg='Black', font=('Ariel', 20))
     label.pack(padx=10, pady=10)
     ftp = tk.Button(first, text="FTP", font=('Ariel', 20), bg='Green', width=40, command=ftp_client)
     ftp.pack(padx=10, pady=30)
-    dns = tk.Button(first, text="DNS", font=('Ariel', 20), bg='Green', width=40, command=ftp_client)
+    dns = tk.Button(first, text="DNS", font=('Ariel', 20), bg='Green', width=40, command=dns_client)
     dns.pack(padx=10, pady=30)
     dhcp = tk.Button(first, text="DHCP", font=('Ariel', 20), bg='Green', width=40, command=ftp_client)
     dhcp.pack(padx=10, pady=30)
+    # first finish
+
+    # ftp window start
     ftpw = Frame(root, bg='Black')
     ftpl = tk.Label(ftpw, text="File Transfer", bg='Black', fg='Green', font=('David', 30))
     ftpl.pack(padx=10, pady=30)
@@ -311,6 +384,9 @@ if __name__ == '__main__':
     uplo.pack(padx=10, pady=10)
     menu = tk.Button(ftpw, text="Menu", font=('Ariel', 10), bg='Red', width=5, command=home)
     menu.pack(padx=10, pady=0)
+    # ftp window finish
+
+    # download window start
     downw = Frame(root, bg='Black')
     downl = tk.Label(downw, text="Download, choose file: ", bg='Black', fg='Green', font=('David', 30))
     downl.pack()
@@ -320,9 +396,28 @@ if __name__ == '__main__':
     cho = tk.Button(downw, text="Enter", font=('Ariel', 20), bg='Green', width=30, command=cfile)
     cho.place(x=50, y=150)
     cho.pack(padx=10, pady=10)
+    # download window finish
+
+    # connection start
     con = Frame(root, bg='Black')
     lab = tk.Label(con, text="Try to connect...please wait", bg='Black', fg='Green', font=('David', 30))
     lab.pack(padx=10, pady=20)
     lab2 = tk.Label(con, text="Can't connect to server, Goodbye", bg='Black', fg='Green', font=('Ariel', 20))
     menu2 = tk.Button(con, text="Menu", font=('Ariel', 10), bg='Red', width=5, command=home)
+    # connection finish
+
+    # DNS window start
+    dnsw = Frame(root, bg='Black')
+    lab = tk.Label(dnsw, text="Enter domain please:", bg='Black', fg='Green', font=('David', 30))
+    lab.pack(padx=10, pady=20)
+    link = tk.Text(dnsw, width=50, height=1, font=('Ariel', 10))
+    link.pack(padx=10, pady=20)
+    domainc = tk.Button(dnsw, text="Domain calculation", font=('Ariel', 20), bg='Green', width=20, command=domain_to_ip)
+    domainc.pack(padx=10, pady=20)
+    showdo = tk.Label(dnsw, text="Ip address -> ", bg='Black', justify=LEFT, fg='Green', font=('Ariel', 20))
+    showdo.pack(padx=10, pady=20)
+    menu3 = tk.Button(dnsw, text="Menu", font=('Ariel', 10), bg='Red', width=5, command=home)
+    menu3.pack(padx=10, pady=20)
+    # DNS window end
+
     root.mainloop()
